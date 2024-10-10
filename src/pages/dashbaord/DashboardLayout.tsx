c8cb6efdc7d602d6184b9e8bafdc68f2971d8164
Ramon Rasheed <ramonrash2@gmail.com>
2024-10-10 17:16:52 +0100

    Fix sidebar open

--- src/pages/dashbaord/DashboardLayout.tsx
+++ src/pages/dashbaord/DashboardLayout.tsx
@@ -1,10 +1,12 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarLayout } from "@/components/ui/sidebar";
+import { useIsMobile } from "@/hooks/useIsMobile";
 import { Outlet } from "react-router-dom";
 
 export default function DashboardLayout() {
+  const isMobile = useIsMobile();
   return (
-    <SidebarLayout defaultOpen={false} className="relative">
+    <SidebarLayout defaultOpen={isMobile ? false : true} className="relative">
       <AppSidebar />
       <main className="flex flex-1 bg-[#F8FAFC] w-full p-2 transition-all duration-300 ease-in-out">
         <Outlet />
